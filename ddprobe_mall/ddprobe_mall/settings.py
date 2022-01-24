246fe1c6652c7931b297d87c9d0ce000a7630aa3
Chenyin Zhang <zhangcheny99@gmail.com>
2022-01-24 09:24:17 +0000

    Add MySQL, Redis, Log support in Django

--- ddprobe_mall/ddprobe_mall/settings.py
+++ ddprobe_mall/ddprobe_mall/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+DB_SETTING_PATH = 'ddprobe_mall/utils/databaseSettings.json'
+DB_NAME, DB_USER, DB_SECRET ='', '', ''
+
+with open(DB_SETTING_PATH) as json_file:
+    dbSetting = json.load(json_file)['databaseSetting']
+    DB_NAME = dbSetting['databaseName']
+    DB_USER = dbSetting['User']
+    DB_SECRET = dbSetting['Secret']
+    
+
 SECRET_KEY = 'django-insecure-m8ncbv_jds#49qj$$n^szlcqug08b7mn14d9-21x2ftkadk(!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -75,10 +87,40 @@ WSGI_APPLICATION = 'ddprobe_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST' : 'localhost',
+        'PORT' : 3306,
+        'USER' : DB_USER,
+        'PASSWORD' : DB_SECRET,
+        'NAME': DB_NAME,
     }
 }
+
+# Cache
+# Use Django-redis to include redis into the project
+# https://github.com/jazzband/django-redis
+CACHES = {
+    # Reserve
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # Used to save the session data
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Use django-redis as backend for session storage
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
 
 
 # Password validation
@@ -121,3 +163,68 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging set up
+# Extracted from https://docs.djangoproject.com/en/4.0/topics/logging/ final complex setup
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {     # The format of our logs
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'filters': {
+        # 'special': {
+        #     '()': 'project.logging.SpecialFilter',
+        #     'foo': 'bar',
+        # },
+        'require_debug_true': {     # only output the log under debug mode for Django
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {        # Write the logs into the console
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {       # Write the logs into specific file
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/mall.log'),
+            'maxBytes': 100 * 1024 * 1024,      # set the max size to 300 MB
+            'backupCount': 10,      # at most contains 10 files
+            'formatter': 'verbose'
+        }
+        # 'mail_admins': {        # mail the error to the admin's email
+        #     'level': 'ERROR',
+        #     'class': 'django.utils.log.AdminEmailHandler',
+        #     'filters': ['special']
+        # }
+    },
+    'loggers': {
+        'django': {     # logger name
+            'handlers': ['console', 'file'],    # are allowed to send log to console and file
+            'propagate': True,
+            'level': 'INFO'
+        }
+        # 'django.request': {
+        #     'handlers': ['mail_admins'],
+        #     'level': 'ERROR',
+        #     'propagate': False,
+        # },
+        # 'myproject.custom': {
+        #     'handlers': ['console', 'mail_admins'],
+        #     'level': 'INFO',
+        #     'filters': ['special']
+        # }
+    }
+}
